151d2ffbafb757bb1241f74745889a81904808b9
Lucotecia <lutecium.uni@gmail,com>
2019-09-04 15:45:54 +0900

    add relational operators

--- 9cc.c
+++ 9cc.c
@@ -24,6 +24,7 @@ struct Token {
   Token *next;    //次の入力トークン
   int val;        //kindがTK_NUMの場合、その数値
   char *str;      //トークン文字列
+  int len;        //トークンの長さ
 };
 
 //現在着目しているトークン
@@ -35,7 +36,11 @@ typedef enum {
   ND_SUB, // -
   ND_MUL, // *
   ND_DIV, // /
-  ND_NUM, // 整数
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // integer
 } NodeKind;
 
 typedef struct Node Node;
@@ -70,8 +75,10 @@ void error_at(char *loc, char *fmt, ...) {
 
 //次のトークンが期待している記号の時はトークンを1つ読み進めてtrueを返す
 //それ以外はfalseを返す
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED ||
+	  strlen(op) != token->len ||
+	  memcmp(token->str, op, token->len))
 	return false;
   token = token->next;
   return true;
@@ -79,9 +86,11 @@ bool consume(char op) {
 
 //次のトークンが期待している記号の時はトークンを1つ読み進める
 //それ以外の場合にはエラーを報告する
-void expect(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-	error_at(token->str,"'%c'ではありません", op);
+void expect(char *op) {
+  if (token->kind != TK_RESERVED ||
+	  strlen(op) != token->len ||
+	  memcmp(token->str, op, token->len))
+	error_at(token->str,"expected \"%s\"", op);
   token = token->next;
 }
 
@@ -89,7 +98,7 @@ void expect(char op) {
 //それ以外の場合にはエラーを報告する
 int expect_number() {
   if (token->kind != TK_NUM)
-	error_at(token->str, "数ではありません");
+	error_at(token->str, "expected a number");
   int val = token->val;
   token = token->next;
   return val;
@@ -100,12 +109,17 @@ bool at_eof() {
 }
 
 //新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
   Token *tok = calloc(1,sizeof(Token));
   tok->kind = kind;
   tok->str = str;
+  tok->len = len;
   cur->next = tok;
   return tok;
+}
+
+bool startswith(char *p, char *q) {
+  return memcmp(p, q, strlen(q)) == 0;
 }
 
 //入力文字列pをトークナイズしてそれを返す
@@ -120,32 +134,50 @@ Token *tokenize(char *p) {
 	  continue;
 	}
 
-	if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
-	  cur = new_token(TK_RESERVED , cur, p++);
+	if (startswith(p, "==") || startswith(p, "!=") ||
+		startswith(p, "<=") || startswith(p, ">=")) {
+	  cur = new_token(TK_RESERVED, cur, p, 2);
+	  p += 2;
+	  continue;
+	}
+	
+	if (*p == '+' || *p == '-' ||
+		*p == '*' || *p == '/' ||
+		*p == '(' || *p == ')' ||
+		*p == '<' || *p == '>' ) {
+	  cur = new_token(TK_RESERVED , cur, p++,1);
 	  continue;
 	}
 
 	if (isdigit(*p)) {
-	  cur = new_token(TK_NUM, cur, p);
+	  cur = new_token(TK_NUM, cur, p, 0);
+	  char *q = p;
 	  cur->val = strtol(p, &p, 10);
+	  cur->len = p - q;
 	  continue;
 	}
-	fprintf(stderr, "loop\n");
+	
 	error_at(token->str, "トークナイズできません");
   }
-  new_token(TK_EOF, cur, p);
+  
+  new_token(TK_EOF, cur, p, 0);
   return head.next;
 }
 
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-  Node *node = calloc(1 ,sizeof(Node));
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
   node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
   node->lhs = lhs;
   node->rhs = rhs;
   return node;
 }
 
-Node *new_node_num(int val) {
+Node *new_num(int val) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = ND_NUM;
   node->val = val;
@@ -155,44 +187,76 @@ Node *new_node_num(int val) {
 Node *expr();//相互再帰のための関数宣言
 
 Node *primary(){
-  if(consume('(')) {
+  if(consume("(")) {
 	  Node *node = expr();
-	  expect(')');
+	  expect(")");
 	  return node;
 	}
-  return new_node_num(expect_number());
+  return new_num(expect_number());
 }
 
 Node *unary() {
-  if (consume('+'))
-	return primary();
-  if (consume('-'))
-	return new_node(ND_SUB, new_node_num(0), primary());
+  if (consume("+"))
+	return unary();
+  if (consume("-"))
+	return new_binary(ND_SUB, new_num(0), unary());
   return primary();
 }
 
 Node *mul() {
   Node *node = unary();
   while(true) {
-	if (consume('*'))
-	  node = new_node(ND_MUL, node, unary());
-	else if (consume('/'))
-	  node = new_node(ND_DIV, node, unary());
+	if (consume("*"))
+	  node = new_binary(ND_MUL, node, unary());
+	else if (consume("/"))
+	  node = new_binary(ND_DIV, node, unary());
+	else
+	  return node;
+  }
+}
+
+Node *add() {
+  Node *node = mul();
+  while(true) {
+	if (consume("+"))
+	  node = new_binary(ND_ADD, node, mul());
+	else if (consume("-"))
+	  node = new_binary(ND_SUB, node, mul());
+	else
+	  return node;
+  }
+}
+
+Node *relational() {
+  Node *node = add();
+  while(true) {
+	if (consume("<"))
+	  node = new_binary(ND_LT, node, add());
+	else if (consume("<="))
+	  node = new_binary(ND_LE, node, add());
+	else if (consume(">"))
+	  node = new_binary(ND_LT, add(), node);
+	else if (consume(">="))
+	  node = new_binary(ND_LE, add(), node);
+	else
+	  return node;
+  }
+}
+
+Node *equality() {
+  Node *node = relational();
+  while(true) {
+	if (consume("=="))
+	  node = new_binary(ND_EQ, node, relational());
+	else if (consume("!="))
+	  node = new_binary(ND_NE, node, relational());
 	else
 	  return node;
   }
 }
 
 Node *expr() {
-  Node *node = mul();
-  while(true) {
-	if (consume('+'))
-	  node = new_node(ND_ADD, node, mul());
-	else if (consume('-'))
-	  node = new_node(ND_SUB, node, mul());
-	else
-	  return node;
-  }
+  return equality();
 }
 
 void gen(Node *node) {
@@ -221,16 +285,34 @@ void gen(Node *node) {
 	printf("  cqo\n");
 	printf("  idiv rax, rdi\n");
 	break;
+  case ND_EQ:
+	printf("  cmp rax, rdi\n");
+	printf("  sete al\n");
+	printf("  movzb rax, al\n");
+	break;
+  case ND_NE:
+	printf("  cmp rax, rdi\n");
+	printf("  setne al\n");
+	printf("  movzb rax, al\n");
+	break;
+  case ND_LT:
+	printf("  cmp rax, rdi\n");
+	printf("  setl al\n");
+	printf("  movzb rax, al\n");
+	break;
+  case ND_LE:
+	printf("  cmp rax, rdi\n");
+	printf("  setle al\n");
+	printf("  movzb rax, al\n");
+	break;
   }
 
   printf("  push rax\n");
 }
 
 int main(int argc, char **argv){
-  if(argc != 2) {
+  if(argc != 2)
     error("引数の個数が不正です\n");
-    return 1;
-  }
 
   //user_inputに入力プログラムを保存
   user_input = argv[1];
